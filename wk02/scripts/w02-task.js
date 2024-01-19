3a63e7bfc6fb3e87fdcac2cc0e9420a5219f2b4d
Jarom Anderson <117554581+jand19081@users.noreply.github.com>
2024-01-19 11:14:03 -0700

    wk2 content added to js file

---
+++ wk02/scripts/w02-task.js
@@ -0,0 +1,48 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+const fullName = 'Jarom Anderson';
+const currentYear = 2024;
+const profilePicture = 'images/linkedinprofile.jpg';
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name')
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`)
+
+
+/* Step 5 - Array */
+
+let favoriteFoods = [' Pizza', ' Rice & Beans', ' Chicken & Rice', ' Bean Dip'];
+
+foodElement.textContent = favoriteFoods;
+
+favoriteFoods.push(' waffles');
+
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+
+favoriteFoods.shift();
+
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+
+favoriteFoods.pop();
+
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+
+
+
+
+
