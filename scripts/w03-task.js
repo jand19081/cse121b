72a9591186b337aedfcf3096fca22c4cc8a4fabf
Jarom Anderson <117554581+jand19081@users.noreply.github.com>
2024-01-23 11:31:40 -0700

    implemented functions

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,17 +1,42 @@
 /* LESSON 3 - Programming Tasks */
-
 /* FUNCTIONS */
-/* Function Definition - Add Numbers */
+function add(number1, number2) {
+    return number1 + number2;
+}
 
+const subtract = function (subtractNumber1, subtractNumber2) { return subtractNumber1 - subtractNumber2 };
+
+const multiply = (factor1, factor2) => { return factor1 * factor2 };
+
+const divide = (divdend, divisor) => { return divdend / divisor };
+
+/* Function Definition - Add Numbers */
+function addNumbers() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
 
 /* Function Expression - Subtract Numbers */
-
+function subtractNumbers() {
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+}
 
 /* Arrow Function - Multiply Numbers */
-
+function multiplyNumbers() {
+    let factorNumber1 = Number(document.querySelector('#factor1').value);
+    let factorNumber2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiply(factorNumber1, factorNumber2);
+}
 
 /* Open Function Use - Divide Numbers */
-
+function divideNumbers() {
+    let divdendNumber = Number(document.querySelector('#dividend').value);
+    let divisorNumber = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(divdendNumber, divisorNumber);
+}
 
 /* Decision Structure */
 
@@ -28,3 +53,9 @@
 /* Output Multiplied by 2 Array */
 
 /* Output Sum of Multiplied by 2 Array */
+
+/* EVENT LISTENERS */
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
