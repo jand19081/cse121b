9dd7e08f97e2e203d6a75f1fb78febee4d3689e2
Jarom Anderson <and19081@gmail.com>
2024-03-16 19:45:26 -0600

    finished task 3

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -40,22 +40,50 @@ function divideNumbers() {
 
 /* Decision Structure */
 
+function getTotal() {
+    let subtotal = Number(document.querySelector('#subtotal').value);
+    const isMember = Boolean(document.querySelector('#member').checked);
+    if (isMember)
+    {
+        subtotal = subtotal * 0.8;
+    }
+
+    document.querySelector('#total').textContent = `$ ${subtotal.toFixed(2)}`
+}
+
 
 /* ARRAY METHODS - Functional Programming */
+const sourceArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13]
+
 /* Output Source Array */
+document.querySelector('#array').textContent = sourceArray;
 
 /* Output Odds Only Array */
+let oddArray = sourceArray.filter(number => { return number % 2 == 1 })
+
+document.querySelector('#odds').textContent = oddArray;
 
 /* Output Evens Only Array */
+let evenArray = sourceArray.filter(number => { return number % 2 == 0 })
 
+document.querySelector('#evens').textContent = evenArray;
 /* Output Sum of Org. Array */
+const arraySum = sourceArray.reduce((total, number) => { return total + number });
 
+
+document.querySelector('#sumOfArray').textContent = arraySum;
 /* Output Multiplied by 2 Array */
+const multipliedArray = sourceArray.map(number => { return number * 2 });
 
+document.querySelector('#multiplied').textContent = multipliedArray;
 /* Output Sum of Multiplied by 2 Array */
+const multipliedSum = sourceArray.map(number => {return number *2}).reduce((total, number) => { return total + number });
+
+document.querySelector('#sumOfMultiplied').textContent = multipliedSum;
 
 /* EVENT LISTENERS */
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+document.querySelector('#getTotal').addEventListener('click', getTotal);
